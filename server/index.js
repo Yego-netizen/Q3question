c99459120f42fbec918ceeba21cdb6a1915dfb28
Yego-netizen <yegogoncalves08@gmail.com>
2026-08-10 00:12:49 -0300

    servindo de forma estática

--- server/index.js
+++ server/index.js
@@ -2,6 +2,8 @@ import express from "express";
 import statsRouter from "./src/routes/stats.routes.js";
 import dotenv from "dotenv";
 import { connectDatabase } from "./src/database/database.js";
+import path from "path";
+import { fileURLToPath } from "url";
 
 dotenv.config();
 
@@ -9,11 +11,21 @@ connectDatabase();
 
 const app = express();
 const port = process.env.PORT || 3000;
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
 app.use(express.json());
 
 app.use("/api/stats", statsRouter);
 
+// Frontend React
+app.use(express.static(path.join(__dirname, "../client/dist")));
+
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "../client/dist/index.html"));
+});
 
 app.listen(port, () => {
-  console.log(`Servidor rodando em http://localhost:${port}`);
+  console.log(`Servidor rodando na porta ${port}`);
 });
