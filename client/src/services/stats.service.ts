888215803e1cd00c813577021ca179f1eddfdd66
Yego-netizen <yegogoncalves08@gmail.com>
2026-08-09 22:36:15 -0300

    atualização do services no front-end

--- client/src/services/stats.service.ts
+++ client/src/services/stats.service.ts
@@ -1,26 +1,21 @@
 import axios from "axios";
 
-const baseUrl = "http://localhost:3000";
+axios.defaults.baseURL =
+  import.meta.env.MODE === "development" ? "http://localhost:3000/api" : "";
 
 export interface StatsDTO {
   nome: string;
   pontuacao: number;
 }
 
-export async function sendStats(data:StatsDTO) {
-    const response = await axios.post(`${baseUrl}/api/stats`, data);
+export async function sendStats(data: StatsDTO) {
+  const response = await axios.post("/stats", data);
 
-    if (!response) return new Error("Erro no envio dos stats");
-
-    return response.data;
-
+  return response.data;
 }
 
 export async function getStats() {
-  const response = await axios.get(`${baseUrl}/api/stats`)
-
-  if (!response) return new Error("Erro no acesso dos stats");
+  const response = await axios.get("/stats");
 
   return response.data;
-  
 }
